86f4639102218979eaebb0e19f0ef941c7afee69
Jongmyung Lee <bbb7931293@gist.ac.kr>
2022-01-11 19:31:21 +0900

    Complete header file

--- header.h
+++ header.h
@@ -12,11 +12,15 @@ using namespace std;
 
 class Account {
     string accountNumber;
+    string accountInfo;
     int balance;
 
    public:
-    Account(string accountNumber, int balance);
+    Account();
+    Account(string accountNumber, string accountInfo, int balance);
+    ~Account();
     string getAccountNumber() const;
+    string getAccountInfo() const;
     int getBalance() const;
     void setBalance(int balance);
     int depositBalance(int money);
@@ -24,22 +28,33 @@ class Account {
 };
 
 class User {
-    string cardNumber;
-    int pinNumber;
+    string cardNumber, userName;
+    int pinNumber, wrongCount;
     vector<Account> accounts;
 
    public:
-    User(string cardNumber, int pinNumber);
+    User();
+    User(string cardNumber, string userName, int pinNumber);
+    ~User();
     string getCardNumber() const;
+    string getUserName() const;
     int getPinNumber() const;
+    int getWrongCount() const;
     vector<Account> getAccounts() const;
-    void setAccountBalance(int accountNum, int balance);
-    int depositAccountBalance(int accountNum, int money);
-    int withdrawAccountBalance(int accountNum, int money);
+    Account& getAccount(int idx);
+    void addWrongCount();
+    void resetWrongCount();
     void setAccounts(vector<Account> accounts);
     void addAccount(Account a);
     static bool isValidCardNumber(string cardNumber);
     static bool isValidPinNumber(int pinNumber);
 };
+
+map<string, User> getUsers();
+
+int insertCard(map<string, User>& users, User& user);
+int checkPinNumber(User& user);
+int selectAccount(User& user, Account& account);
+int bank(Account& account);
 
 // #endif
